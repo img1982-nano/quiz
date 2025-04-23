07ad0813390672e90b0e4fb41549668094cd0803
img1982-nano <ddoossyyu+img@gmail.com>
2025-04-23 15:51:42 +0000

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+    import { goto } from '$app/navigation';
+</script>
+<h1 class="text-black text-4xl">あじくいず</h1>
+<button onclick={goto("/main")} class="bg-amber-500">スタート</button>
---
+++ src/routes/main/+page.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+    let questions = [
+        {
+        "id": 1,
+        "message": "パンはパンでもだべられないパンは？",
+        "answer": "フライパン"
+        },
+        {
+        "id": 2,
+        "message": "黒ずくめの組織のリーダーは？",
+        "answer": "ジン"
+        },
+        {
+        "id": 3,
+        "message": "MicrosoftのCEOは？",
+        "answer": "マイケル・ジャクソン(嘘)"
+        },
+    ]
+    function next(){
+        if (answer === questions[question].answer) {
+            alert("正解！おめでとう")
+            location.reload()
+        } else {
+            alert(`不正解...正解は:${questions[question].answer}`)
+            location.reload()
+        }
+    }
+    let question: number = Math.floor(Math.random() * 3);
+    let answer = ""
+</script>
+<h1 class="text-black text-mono text-3xl">問題:{questions[question].message}</h1>
+<input bind:value={answer} class="border-blue-500"/>
+<button class="bg-yellow-200 active:bg-yellow-400 rounded x-10 y-30" onclick={() => next()}>答え合わせ</button>
